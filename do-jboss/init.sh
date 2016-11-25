442356150f1ebce02fd7a38ea35691e9bffb2a00
Khakhana Thimachai <khakhana@bluebik.com>
2016-11-26 00:01:39 +0700

    setup swapfile

--- do-jboss/init.sh
+++ do-jboss/init.sh
@@ -4,3 +4,13 @@ sleep 30
 
 yum -y upgrade
 yum -y install vim wget
+
+
+# Setup swap ~2G
+dd if=/dev/zero of=/swapfile bs=1024 count=2048k
+mkswap /swapfile
+swapon /swapfile
+echo "/swapfile          swap            swap    defaults        0 0" >> /etc/fstab
+chown root:root /swapfile 
+chmod 0600 /swapfile
+sysctl vm.swappiness=10 >> /etc/sysctl.conf
